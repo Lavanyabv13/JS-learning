cb91f47989bf3a3d0622a1b1892cad2e1326d716
Lavanyabv13 <lavanyabv11@gmail.com>
2026-07-11 00:18:42 +0530

    Day 1: Created JavaScript project and first Hello World program

---
+++ 01-JavaScript-Basics/Day-01/app.js
@@ -0,0 +1,6 @@
+console.log("Hello, World!");
+console.log("This is my first JavaScript program.");
+console.log("I am learning JavaScript basics.");
+console.log("My name is Lavanya");
+console.log("I am excited to learn more about JavaScript!");
+console.log("I will become a great QA Automation Engineer.");
