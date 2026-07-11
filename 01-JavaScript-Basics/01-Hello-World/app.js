dd5bd5432213436f8bb06183ca5162bd45d0d2cc
Lavanyabv13 <lavanyabv11@gmail.com>
2026-07-11 19:15:33 +0530

    Day 2: Created a 02-Variable folder and files in it

---
+++ 01-JavaScript-Basics/01-Hello-World/app.js
@@ -0,0 +1,11 @@
+console.log("Hello, World!");
+console.log("This is my first JavaScript program.");
+console.log("I am learning JavaScript basics.");
+console.log("My name is Lavanya");
+console.log("I am excited to learn more about JavaScript!");
+console.log("I will become a great QA Automation Engineer.");
+
+/*Why do we run node 01-JavaScript-Basics/Day-01/app.js?
+Because Node.js needs to know which JavaScript file to execute.
+(It's like opening a specific document)
+If your file were directly inside JS-learning, you could run:node app.js */
