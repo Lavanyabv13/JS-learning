90616fc08e17aa3cbd04deb1bc140d6f027a1103
Lavanyabv13 <lavanyabv11@gmail.com>
2026-07-13 01:30:58 +0530

    JS-008 JavaScript data types practice update 2

--- 01-JavaScript-Basics/04-Data-Types/app.js
+++ 01-JavaScript-Basics/04-Data-Types/app.js
@@ -5,22 +5,32 @@
 //1. String
 let name = "Lavanya";
 console.log(name);
+//output: Lavanya
+
 
 //2. Number
 let age = 28;
 console.log(age);
+//output: 28
+
 
 //3. Boolean
 let isemployeed = true;
 console.log(isemployeed);
+//output: true
+
 
 //4. undefined
 let city;
 console.log(city);
+//output: undefined
+
 
 //5. null
 let mobileNumber = null;
 console.log(mobileNumber);
+//output: null
+
 
 //6. Symbol
 let employeeName1 = Symbol("Naresh");
@@ -29,12 +39,16 @@ console.log(employeeName1 === employeeName2);
 console.log(employeeName1);
 console.log(employeeName2); 
 //Symbol is unique and immutable data type. Even if the value is same, it will create a new symbol every time. So, the comparison will return false.
+//output: false
+//output: Symbol(Naresh)
+//output: Symbol(Naresh)
+
 
 //7. BigInt
 let bigIntValue = 1234567890123456789012345678901234567890n;
 console.log(bigIntValue);
 //n at the end of the number indicates that it is a BigInt. 
-
+//output: 1234567890123456789012345678901234567890n
 
 
 //Non-Primitives Data Types
@@ -47,16 +61,22 @@ let employee = {
     city: "Bangalore"
 };
 console.log(employee);
+//output: { name: "Lavanya", age: 28, isemployeed: true, city: "Bangalore" }
+
 
 //2. Array
 let fruits = ["Apple", "Banana", "Mango", "Orange"];
 console.log(fruits);
+//output: ["Apple", "Banana", "Mango", "Orange"]
+
 
 //3. Function
 function greet() {
     console.log("Hello, Welcome to JavaScript!");
 }
 greet();
+//output: Hello, Welcome to JavaScript!
+
 
 //if needed to print a welcome message for 5 times using function
 function welcomeMessage() {
@@ -67,14 +87,21 @@ welcomeMessage();
 welcomeMessage();
 welcomeMessage();
 welcomeMessage();
+//output: Hello, Welcome to home! (5 times will be printed same)
 
 //if needed to print a welcome message for 5 different users same message with different names using function
 function welcomeUser(name) {
-    console.log("Hello " + name + "Welcome to home!");
+    console.log("Hello " + name + ", Welcome to home!");
 }
 welcomeUser("Lavanya");
 welcomeUser("Naresh");
 welcomeUser("Pooja");
 welcomeUser("Mother");
 welcomeUser("Father");
+//output: Hello Lavanya, Welcome to home!
+//output: Hello Naresh, Welcome to home!
+//output: Hello Pooja, Welcome to home!
+//output: Hello Mother, Welcome to home!
+//output: Hello Father, Welcome to home!
+
 
